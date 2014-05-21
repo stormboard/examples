f65c820d98a675af0918f71defd1c6620b1cbe62
Michael Bollman <michael@roxodesigns.com>
2014-05-21 07:39:27 -0600

    Storm Ideas example using lastModifiedMin query filter

--- php/api/storms-ideas.php
+++ php/api/storms-ideas.php
@@ -9,6 +9,11 @@ head("GET: Storms/Ideas");
 
 $id = $_GET["id"];
 
+//Standard Storm Idea GET Request
 get("storms/{$id}/ideas");
+
+//Filter by Greater Than Last Modified Date
+//get("storms/{$id}/ideas?lastModifiedMin=".urlencode("2014-05-10T00:00:00-00:00"));
+
 
 foot();
